3d8a33a7b608dca0579c6e35d91f00281018fb7b
Sudharshan reddy <115392665+sudharshanreddy4567@users.noreply.github.com>
2022-12-18 11:47:49 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,40 @@
+const a=document.getElementById("hey")
+a.innerHTML="hey"
+console.log(a)
+const b=document.getElementById("my")
+b.innerHTML="my"
+console.log(b)
+const c=document.getElementById("K")
+c.innerHTML="K"
+console.log(c)
+const d=document.getElementById("name")
+d.innerHTML="name"
+console.log(d)
+const e=document.getElementById("and")
+e.innerHTML="and"
+console.log(e)
+const f=document.getElementById("a")
+f.innerHTML="a"
+console.log(f)
+const g=document.getElementById("age")
+g.innerHTML="age"
+console.log(g)
+const h=document.getElementById("age")
+h.innerHTML="a"
+console.log(h)
+const i=document.getElementById("i")
+i.innerHTML="i"
+console.log(i)
+const j=document.getElementById("love")
+j.innerHTML="love"
+console.log(j)
+const k=document.getElementById("this")
+k.innerHTML="this"
+console.log(k)
+const l=document.getElementById("sub")
+l.innerHTML="sub"
+console.log(l)
+
+
+
+
